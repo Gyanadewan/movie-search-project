2b330839bead093e554fb513b58ffef1137f1739
Gyanadewan <gyanadewan1@gmail.com>
2026-07-03 22:38:52 +0600

    added menu button toggle

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -3,9 +3,9 @@ import Movie from './Movie/Movie'
 
 function Movies({movies}) {
   return (
-   <div>
-       <h3 className='  flex justify-center m-8 font-bold text-5xl  '>All Movies</h3>
-      <div className='grid grid-cols-4 gap-6'>
+   <div className='px-4 border border-amber-600 m-3'>
+       <h3 className='  flex justify-center m-8 font-bold text-5xl   '>All Movies</h3>
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-6  '>
        {
         movies.map(singleMovie => <Movie singleMovie={singleMovie}></Movie> )
        }
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
 function Navbar() {
+  const [open,setOpen] = useState(false)
   return (
-    <nav className="bg-gray-900 text-white shadow-md">
-      <div className="max-w-6xl mx-auto flex justify-between items-center px-6 py-4">
+    <nav className="bg-gray-900 text-white shadow-md relative">
+      <div className="max-w-6xl mx-auto flex justify-between items-center px-1 md:px-6 py-4">
         {/* Logo */}
         <h2 className="text-3xl font-bold cursor-pointer">
           <span className="text-red-500">Movie</span>
@@ -11,7 +12,7 @@ function Navbar() {
         </h2>
 
         {/* Menu */}
-        <ul className="flex items-center gap-8 font-medium">
+        <ul className=" hidden md:flex items-center gap-8 font-medium">
           <li>
             <a href="/" className="hover:text-red-500 transition duration-300">
               Home
@@ -47,18 +48,40 @@ function Navbar() {
         </ul>
 
         {/* Search Box */}
-        <div className="hidden md:flex">
+        <div className=" hidden md:flex">
           <input
             type="text"
             placeholder="Search movies..."
-            className="px-4 py-2 rounded-l-md text-white outline-none"
+            className=" px-2 md:px-4 py-1 md:py-2 rounded-l-md text-white outline-none"
           />
 
-          <button className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded-r-md transition duration-300">
+          <button className="bg-red-500 hover:bg-red-600  px-4 py-2 rounded-r-md transition duration-300">
             Search
           </button>
         </div>
+
+          {/* mobile menu */}
+        <div className="  md:hidden">
+           <button className="font-bold text-3xl text-white" onClick={()=> setOpen(!open)}> < CiMenuBurger/></button>
+        </div>
+       
       </div>
+       {open && (
+    <ul className="absolute right-4 top-16 w-40 bg-gray-800 rounded-lg shadow-lg py-4 md:hidden">
+      <li className="px-6 py-2 hover:bg-gray-700">
+        <a href="/">Home</a>
+      </li>
+      <li className="px-6 py-2 hover:bg-gray-700">
+        <a href="/movies">Movies</a>
+      </li>
+      <li className="px-6 py-2 hover:bg-gray-700">
+        <a href="/favorites">Favorites</a>
+      </li>
+      <li className="px-6 py-2 hover:bg-gray-700">
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  )}
     </nav>
   );
 }
